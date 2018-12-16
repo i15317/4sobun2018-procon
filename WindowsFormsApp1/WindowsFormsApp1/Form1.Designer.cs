40d614db89d5813665b82636be3f0b98336dd3e1
DESKTOP-8VVHAM9\i15317 <kenta_990815@outlook.jp>
2018-12-17 06:11:05 +0900

    First commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(918, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(324, 138);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(125, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "次のタイマー作動まで";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(549, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 50);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(196, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(466, 48);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ここにループ回数を入力";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(196, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 48);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(125, 418);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(254, 48);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "残りターン数";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(196, 493);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 48);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(125, 563);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 48);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "延長ターン数";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(196, 625);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(135, 48);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(104, 147);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(558, 48);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "延長が発生したらここに入力";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(549, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 50);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(918, 241);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(324, 149);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 709);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(226, 98);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "入場";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(266, 709);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(226, 98);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "カウントダウン";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(526, 709);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(226, 98);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "3,2,1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(779, 709);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(226, 98);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "結果発表";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1031, 709);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(226, 98);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "撤収";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1414, 829);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,217 @@
+﻿using NAudio.Wave;
+using System;
+using System.Media;
+using System.Windows.Forms;
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        //ループ回数
+        int loop_cnt = 0;
+        int additional_cnt = 0;
+        int timeLeft = 0;
+        //これはBGM用
+        private System.Media.SoundPlayer player = null;
+        //5秒前からのアラーム
+        string SoundFile = "count.wav";
+        bool flag = true;
+        bool flag2 = true;
+        WaveOut waveOut = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label7.Text = "0";
+            InitializeTimer();
+        }
+
+        static void timer_Elapsed(object sender, EventArgs e)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                label1.Text = "次のカウントまで";
+                if (timeLeft > 0)
+                {
+                    timeLeft = timeLeft - 1;
+                    label4.Text = timeLeft + " seconds";
+                }
+                else
+                {
+                    timer2.Stop();
+                    label4.Text = "Time's up!";
+                }
+            });
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (int.TryParse(textBox1.Text, out loop_cnt))
+            {
+                button3.Text = "入場";
+                AudioFileReader reader = new AudioFileReader("start.wav");
+                reader.Position = 0;
+                WaveOut takada = new WaveOut();
+                takada.Init(reader);
+                takada.Play();
+                //変換出来たら、dにその数値が入る
+                label6.Text = loop_cnt.ToString();
+                label1.Text = "実行開始";
+                // works(loop_cnt);
+                timer1.Enabled = true;
+                timer2.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("{0} は数字ではありません。", textBox1.Text);
+            }
+        }
+
+        private void InitializeTimer()
+        {
+            //初回は15秒（音声開始時刻）
+            timer1.Interval = 15000;
+            //UIカウント用
+            timer2.Interval = 1000;
+            button2.Text = "start";
+            timer1.Tick += new EventHandler(Timer1_Tick);
+            button1.Click += new EventHandler(button1_Click);
+            timer2.Tick += new EventHandler(timer2_Tick);
+            //1回目
+            timeLeft = 35;
+        }
+
+
+        private void Timer1_Tick(object Sender, EventArgs e)
+        {
+            loop_cnt--;
+            if (flag)
+            {
+                //初回ループのみ
+                label6.Text = loop_cnt.ToString();
+                //オーディオファイル名
+                ////2回目の秒数
+                playSoundEffect();
+                timer1.Interval = 20000;
+                timeLeft = 20;
+                timer2.Enabled = true;
+                flag = false;
+            }
+            else if (loop_cnt > 0)
+            {
+                label6.Text = loop_cnt.ToString();
+                //オーディオファイル名
+                playSoundEffect();
+                timeLeft = 20;
+                timer2.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("終了");
+                timer1.Stop();
+            }
+
+        }
+       
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int tmp_cnt = 0;
+            //延長時間分（０から９までじゃないと挙動がおかしくなるよ）
+            if (int.TryParse(textBox2.Text, out tmp_cnt))
+            {
+                loop_cnt += tmp_cnt;
+                additional_cnt += tmp_cnt;
+                label7.Text = additional_cnt.ToString();
+            }
+        }
+
+        private void playSoundEffect()
+        {
+            AudioFileReader reader = new AudioFileReader(SoundFile);
+            reader.Position = 0;
+            if (waveOut == null)
+            {
+                waveOut = new WaveOut();
+            }
+            waveOut.Init(reader);
+            waveOut.Play();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (player == null)
+            {
+                button2.Text = "Stop";
+                player = new SoundPlayer("bgm2.wav");
+                player.PlayLooping();
+            }
+            else
+            {
+                button2.Text = "Start";
+                player.Stop();
+                player.Dispose();
+                player = null;
+            }
+        }
+        /**
+         * こういう書き方をして激しく後悔してる
+         * 
+         */ 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AudioFileReader reader = new AudioFileReader("entrance.wav");
+            reader.Position = 0;
+            WaveOut takada = new WaveOut();
+            takada.Init(reader);
+            takada.Play();
+        }
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            AudioFileReader reader = new AudioFileReader("countcall.wav");
+            reader.Position = 0;
+
+            WaveOut takada = new WaveOut();
+
+            takada.Init(reader);
+            takada.Play();
+
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            AudioFileReader reader = new AudioFileReader("321.wav");
+            reader.Position = 0;
+            WaveOut takada = new WaveOut();
+            takada.Init(reader);
+            takada.Play();
+
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            AudioFileReader reader = new AudioFileReader("result.wav");
+            reader.Position = 0;
+            WaveOut takada = new WaveOut();
+            takada.Init(reader);
+            takada.Play();
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            AudioFileReader reader = new AudioFileReader("goback.wav");
+            reader.Position = 0;
+            WaveOut takada = new WaveOut();
+            takada.Init(reader);
+            takada.Play();
+        }
+    }
+}
+
